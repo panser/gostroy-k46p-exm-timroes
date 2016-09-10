0a03301c5c30616f25e847c0b9519c168729fdc6
Sergey Panov <gostroy@gmail.com>
2016-09-10 21:52:28 +0300

    part2: refactor, add html

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 module.exports = function(kibana) {
+  // Return a new instance of kibana.Plugin that holds
+  // information about this plugin.
   return new kibana.Plugin({
 
+    // We have some ui components, that we need to describe
     uiExports: {
+      // Register our visualizations (a plugin can have multiple visualizations)
       visTypes: [ 'plugins/gostroy_k_46_p_exm_timroes/clock' ]
     }
 
--- public/clock.js
+++ public/clock.js
@@ -1,6 +1,13 @@
+// public parts of the plugin (i.e. parts that reside in the public folder and will be transfered to the client)
+// must be AMD modules (RequireJS)
 define(function(require) {
+
+    // The provider function must return the visualization
     function ClockProvider(Private) {
+        // Load TemplateVisType
         var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
+
+        // Return a new instance describing this visualization
         return new TemplateVisType({
             name: 'gostroyClock', // the internal id of the visualization
             title: 'Clock', // the name shown in the visualize list
@@ -11,7 +18,11 @@ define(function(require) {
         });
     }
 
+    // Register the above provider to the visualization registry
     require('ui/registry/vis_types').register(ClockProvider);
 
+    // Return the provider, so you potentially load it with RequireJS.
+    // This isn't mandatory, but since all Kibana plugins do this, you might
+    // want to also return the provider.
     return ClockProvider;
 });
